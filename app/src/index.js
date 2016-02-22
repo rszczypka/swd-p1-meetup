3c7f5452dacfa2fb97b34bd6a367b38101727748
Rafal Szczypka <rszczypka@gmail.com>
2016-02-22 02:25:27 +0000

    font-awesome included from npm and not from cdn

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,7 @@ import router from './utils/router';
 import store from './stores';
 import { restore } from 'actions/auth';
 import 'react-fastclick';
+import 'font-awesome/css/font-awesome.min.css';
 
 
 store.dispatch(restore());
